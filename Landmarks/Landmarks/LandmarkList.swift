a978e7c6a9c208dd2669f9cd51166d8675accd82
baris dilekci <bdilekci200@gmail.com>
2023-07-28 15:40:30 +0300

    feat: create landmarklist

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Baris on 28.07.2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+                   LandmarkRow(landmark: landmark)
+               }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
---
+++ Landmarks/Views/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Landmarks
+//
+//  Created by Baris on 28.07.2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
