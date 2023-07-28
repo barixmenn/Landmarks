ac8f34a8f2a67c13510b6386d43a716fdeb68332
baris dilekci <bdilekci200@gmail.com>
2023-07-28 22:10:32 +0300

    feat: create hikegrapch

---
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -0,0 +1,20 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by Baris on 28.07.2023.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
